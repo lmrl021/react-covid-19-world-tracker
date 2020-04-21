65f6dd9b1a918c7884050de8b0a9bdc34ec60272
lmrl021 <lmarlsaria21@gmail.com>
2020-04-21 12:52:39 +0800

    Updates

---
+++ precache-manifest.788d4aa02d277116b16f04018adec06a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8bac7ddf9f1c017a37003e431feef8",
+    "url": "/react-covid-19-world-tracker/index.html"
+  },
+  {
+    "revision": "d340225fc45fcd18faa6",
+    "url": "/react-covid-19-world-tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1bf491162992277cbc4f",
+    "url": "/react-covid-19-world-tracker/static/css/main.9eb54fc3.chunk.css"
+  },
+  {
+    "revision": "d340225fc45fcd18faa6",
+    "url": "/react-covid-19-world-tracker/static/js/2.2290b7d3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-covid-19-world-tracker/static/js/2.2290b7d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf491162992277cbc4f",
+    "url": "/react-covid-19-world-tracker/static/js/main.3e5f4a7d.chunk.js"
+  },
+  {
+    "revision": "726eda5a39aa8abd9773",
+    "url": "/react-covid-19-world-tracker/static/js/runtime-main.650b1075.js"
+  }
+]);
